707b548aa3b0ebc5a5ba01c413c8ff76ac15bf6d
John <joka527@gmail.com>
2022-07-27 15:15:00 +0900

    enable cors server side through expressjs

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/angular-leaflet-starter'));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/angular-leaflet-starter/index.html'));});
