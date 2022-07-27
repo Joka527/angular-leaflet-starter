dcaac608ab3fd3ba92943c4df49799b7a18b9bb1
John <joka527@gmail.com>
2022-07-27 15:24:10 +0900

    actually try using cors npm package this time

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.static(__dirname + '/dist/angular-leaflet-starter'));
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/angular-leaflet-starter/index.html'));});
